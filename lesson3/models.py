9fd03f7c4c0bed253c12a475d50087d16c641ee8
gpdzvapatsva <gpdzvapatsva@gmail.com>
2024-10-10 15:01:46 +0100

    displaying records

--- lesson3/models.py
+++ lesson3/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    firstname=models.CharField(max_length=30)
+    surname=models.CharField(max_length=30)
+    student_id=models.IntegerField()
+    course=models.CharField(max_length=30)
+
+    def __str__(self):
+        return (f"ID: {self.id} firstname {self.firstname},"
+                f"surname {self.surname} Student ID {self.student_id},"
+                f"Course : {self.course}")
--- lesson3/views.py
+++ lesson3/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from lesson3.models import Students
 
 # Create your views here.
 def index(request):
     return render (request, 'index.html')
 
 def records(request):
-    return render (request, 'records.html')
+    students=Students.objects.all()
+    context={'students':students}
+    return render (request, 'records.html', context)
 def contacts(request):
     return render (request, 'contact.html')
